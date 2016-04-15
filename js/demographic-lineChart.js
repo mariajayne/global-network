a45f5e5b072dd34f17275b49f6fcbadfd0bd32e1
aksel.reiten <aksel.reiten@gmail.com>
2016-04-15 18:29:37 -0400

    implemented tool-tips

--- js/demographic-lineChart.js
+++ js/demographic-lineChart.js
@@ -2,8 +2,6 @@
  * Created by akselreiten on 15/04/16.
  */
 
-var commasFormatter = d3.format(",.0f");
-
 var metricMap = {
     "gdp" : "GDP (current LCU)",
     "internet" : "Internet users (per 100 people) ",
@@ -12,10 +10,20 @@ var metricMap = {
 }
 
 var labelMap = {
-    "gdp" : "$",
-    "internet" : "%",
-    "unemployment" : "%",
-    "university" : "%"
+    "gdp" : "$USD",
+    "internet" : "% of population",
+    "unemployment" : "% of work force",
+    "university" : "% of population"
+}
+
+var formatValue = d3.format(".2s");
+var formatPercent = d3.format(".0%");
+var formatYear = d3.time.format("%Y");
+var bisectDate = d3.bisector(function(d) { return d.year; }).left;
+
+function formatYLabel(point, metric){
+    if (metric == 'gdp'){return formatValue(point)}
+    else if (metric == 'internet' || 'unemployment' || 'university'){return formatPercent(point/100)}
 }
 
 LineChart= function(_parentElement,_data,_metric){
@@ -23,15 +31,13 @@ LineChart= function(_parentElement,_data,_metric){
     this.data = _data;
     this.displayData = [];
     this.metric = _metric;
-
-
     this.initVis();
 }
 
 LineChart.prototype.initVis = function(){
     var vis = this;
 
-    //  Defining margins
+    //  Defining margins, height and width
     vis.margin = {top:30,right:20,bottom:20,left:100}
     vis.width = 600 - vis.margin.left - vis.margin.right;
     vis.height = 300 - vis.margin.top - vis.margin.bottom;
@@ -45,7 +51,6 @@ LineChart.prototype.initVis = function(){
 
     // Scales
     vis.x = d3.time.scale().range([0, vis.width]);
-
     vis.y = d3.scale.linear().range([vis.height, 0]);
 
     //  Axis
@@ -57,6 +62,7 @@ LineChart.prototype.initVis = function(){
     vis.yAxis = d3.svg.axis()
         .scale(vis.y)
         .orient("left")
+        .tickFormat(function(d){return formatYLabel(d, vis.metric)})
         //.tickFormat(function(d) { return "$" + commasFormatter(d/1000000) + "M"; })
         .ticks(7);
 
@@ -92,7 +98,7 @@ LineChart.prototype.initVis = function(){
         .attr("transform", "rotate(0)")
         .text(labelMap[vis.metric]);
 
-    //  Appending title
+    //  Appending main title
     vis.svg.append("text")
         .attr("class", "axis-title")
         .attr("text-anchor", "middle")
@@ -102,13 +108,79 @@ LineChart.prototype.initVis = function(){
         .attr("transform", "rotate(0)")
         .text(metricMap[vis.metric]);
 
+    //  Appending line and circle for tool-tips
+    vis.focus = vis.svg.append("g").style("display","none");
+
+    vis.focus.append("circle")
+        .attr("class","y")
+        .style("fill","#58B957")
+        .style("stroke","gray")
+        .attr("r",5);
+
+    vis.focus.append("line")
+        .attr("class","x")
+        .style("stroke","black")
+        .style("stroke-dasharray","10,5")
+        .style("opacity",0.9)
+        .attr("y1",0)
+        .attr("y2",vis.height);
+
+    vis.focus.append("text")
+        .attr("class", "y1")
+        .style("stroke", "black")
+        .style("font-size",14)
+        .style("stroke-width", "1px")
+        .style("opacity", 0.8)
+        .attr("dx", 8)
+        .attr("dy", "-.3em");
+
+    vis.focus.append("text")
+        .attr("class", "y2")
+        .style("stroke", "gray")
+        .style("font-size",12)
+        .style("stroke-width", "0.75px")
+        .style("opacity", 0.8)
+        .attr("dx", 8)
+        .attr("dy", "-.3em");
+
+    //Append rectangle for monitoring pointer-events
+    vis.svg.append("rect")
+        .attr("width",vis.width)
+        .attr("height",vis.height)
+        .style("fill","none")
+        .style("pointer-events","all")
+        .on("mouseover",function(){vis.focus.style("display",null);})
+        .on("mouseout",function(){vis.focus.style("display","none");})
+        .on("mousemove",mouseMove);
+
+    function mouseMove() {
+
+        var x0 = vis.x.invert(d3.mouse(this)[0])
+        var i = bisectDate(vis.displayData, x0, 1),
+            d0 = vis.displayData[i - 1],
+            d1 = vis.displayData[i],
+            d = x0 - d0.year > d1.year- x0 ? d1 : d0;
+
+        vis.focus.select("circle.y")
+            .attr("transform","translate(" + vis.x(d.year) + "," + vis.y(d[vis.metric]) + ")");
+        vis.focus.select("line.x")
+            .attr("transform","translate(" + vis.x(d.year) + "," + vis.height + ")")
+            .attr("y1", 0).attr("y2", -vis.height);
+        vis.focus.select("text.y1")
+            .attr("transform","translate(" + vis.x(d.year) + "," + 35 + ")")
+            .text("Value: " + formatYLabel(d[vis.metric],vis.metric));
+        vis.focus.select("text.y2")
+            .attr("transform","translate(" + vis.x(d.year) + "," + 50 + ")")
+            .text("Year: " + formatYear(d.year));
+    }
+
     vis.wrangleData();
 }
 
 
 LineChart.prototype.wrangleData = function(){
     var vis = this;
-    var countryScope = "SWE"    //  Make this listen to map.
+    var countryScope = "USA"    //  Make this listen to map.
     vis.displayData = vis.data.countries.filter(function(d){return (d.country_id == countryScope)})[0].years
     vis.updateVis();
 }
@@ -134,6 +206,7 @@ LineChart.prototype.updateVis = function(){
     linegraph
         .style("stroke",2)
         .attr("d",vis.line(vis.displayData));
+
 
 
 }
