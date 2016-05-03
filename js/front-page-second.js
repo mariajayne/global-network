2d2fc052a30c763812926ba8903c7f015732e287
aksel.reiten <aksel.reiten@gmail.com>
2016-05-02 21:39:51 -0400

    Removed blue background on 'home'-page, except for the first time. Stored it in 'frontpage.html'

--- js/front-page-second.js
+++ js/front-page-second.js
@@ -1,10 +1,7 @@
-/**
- * Created by MagnusMoan on 17/04/16.
- */
-
 
 // Hide navbar until animation starts
 document.getElementById("frontpage-headline").style.visibility = "hidden";
+
 
 var map = [],
     demographicData = [],
@@ -13,27 +10,22 @@ var map = [],
 loadData();
 
 function loadData() {
-
     queue()
-        .defer(d3.json, "data/demographics/world-topo.json")
-        .defer(d3.json, "data/nodeMap/Test.json")
+        .defer(d3.json, "../data/demographics/world-topo.json")
+        .defer(d3.json, "../data/nodeMap/Test.json")
         .await(processData);
-
 }
 
 function processData(error,data1,data2){
     map = data1;
     demographicData = data2.slice(0,-1);
     globalNumberOfUsers = data2.slice(-1);
-    document.getElementById("launchButton").addEventListener("click", createVis);
+    createVis();
 }
 
 function createVis() {
     document.getElementById("frontpage-headline").style.visibility = "visible";
-    document.getElementById("undercover").style.visibility = "hidden";
-    document.getElementById("launchButton").style.visibility = "hidden";
     d3.select("#cover").transition().duration(2000).style("opacity", 0);
-    setTimeout(function() { document.getElementById("cover").style.visibility = "hidden";}, 2000);
     worldMap = new WorldMap("map",map,demographicData, globalNumberOfUsers);
 }
 
